0d7261ce30e307abf315be51ed7e6570f806ca63
Sarah-Aly <saaly@microsoft.com>
2022-02-09 16:34:47 -0800

    change port number to 8081
    To mitigate this error: our container is exposing port 8080, but your app is actually listening on port 8081.

--- apis/trips/main.go
+++ apis/trips/main.go
@@ -11,7 +11,7 @@ import (
 )
 
 var (
-	webServerPort    = flag.String("webServerPort", getEnv("WEB_PORT", "8080"), "web server port")
+	webServerPort    = flag.String("webServerPort", getEnv("WEB_PORT", "8081"), "web server port")
 	webServerBaseURI = flag.String("webServerBaseURI", getEnv("WEB_SERVER_BASE_URI", "changeme"), "base portion of server uri")
 )
 
